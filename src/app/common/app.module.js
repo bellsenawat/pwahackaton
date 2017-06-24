41669b2a2a37b0450e402fb307eaf25b5839bc9b
Thanwat Senawat <bell.senawat@gmail.com>
2017-06-25 04:06:19 +0700

    start implement design

---
+++ src/app/common/app.module.js
@@ -0,0 +1,23 @@
+import uiRouter from 'angular-ui-router';
+import { appComponent } from './app.component';
+import { appNav } from './app-nav/app-nav.module';
+import { appSidebar } from './app-sidebar/app-sidebar.module';
+import './app.scss';
+
+export const app = angular
+  .module('common.app', [
+    uiRouter,
+    appNav,
+    appSidebar,
+  ])
+  .component('app', appComponent)
+  .config(($stateProvider) => {
+    'ngInject';
+
+    $stateProvider
+      .state('app', {
+        url: '/',
+        component: 'app',
+      });
+  })
+  .name;
--- src/app/root.module.js
+++ src/app/root.module.js
@@ -1,12 +1,14 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import { rootComponent } from './root.component';
+import { common } from './common/common.module';
 import './root.scss';
 
 
 export const root = angular
   .module('root', [
     uiRouter,
+    common
   ])
   .component('root', rootComponent)
   .config(($locationProvider, $urlRouterProvider) => {
