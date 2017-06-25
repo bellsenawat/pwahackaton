edecf6f3a2b97d39b6f9e7a6a0ff2ee5c08186bd
Thanwat Senawat <bell.senawat@gmail.com>
2017-06-25 19:45:56 +0700

    Done version 0.0.0.1

--- src/app/common/app.component.js
+++ src/app/common/app.component.js
@@ -3,16 +3,20 @@ import templateUrl from './app.html';
 export const appComponent = {
   templateUrl,
   controller: class AppComponent {
-    constructor($state, AssistantService) {
+    constructor($state, AssistantService, $window) {
       'ngInject';
       this.$state = $state;
       this.assistantService = AssistantService;
+      this.$window = $window;
     }
 
     $onInit() {
+      this.todlList = [];
+      this.todlListActive = false;
       if(this.assistantService.isUserIntroYet() ) {
         this.isCompleteIntro = true;
         this.assistantSection = true;
+        this.user = this.assistantService.getUser();
         this.sayHiUser();
       } else {
         this.isCompleteIntro = false;
@@ -32,6 +36,36 @@ export const appComponent = {
        this.sayHi = this.assistantService.sayHi();
     }
 
+    serachFood() {
+      let url = 'https://www.wongnai.com/businesses?q=' + this.searchtext;
+      this.openInNewTab(url)
+    }
+
+    serachFlight() {
+      let url = 'https://www.google.co.th/search?q=site+traveloka.com+' + this.searchtext;
+      this.openInNewTab(url)
+    }
+
+    serachTodo() {
+      this.todlListActive = true;
+      let list = this.searchtext;
+      this.todlList.push(list);
+    }
+
+    needToSleep() {
+      this.message = 'จะไปนอนแล้วหรอคะ ' + this.user.name;
+      this.modalSleepOpen = true;
+    }
+
+    confirmSleep() {
+      this.message = 'กรุณาปิดเว็บแล้วไปนอนนะ ฝันดีนะคะ';
+    }
+                    
+
+    openInNewTab(url) {
+      var win = window.open(url, '_blank');
+      win.focus();
+    }
     
 
   },
